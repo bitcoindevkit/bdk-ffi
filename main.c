c87a8e1c51a91fa789943b667eca0996483188bd
Steve Myers <steve@notmandatory.org>
2021-06-04 23:01:51 -0700

    Pass wallet by ref

--- main.c
+++ main.c
@@ -12,24 +12,32 @@ int main (int argc, char const * const argv[])
     print_string(string3);
     print_string(string3);
     free_string(string3);
+    //free_string(string3);
 
     //Point_t a = new_point(84,45);
     //Point_t b = new_point(0,39);
     //Point_t m = mid_point(a, b);
     //print_point(m);
     
-    //char const * name = "test_wallet";
-    //char const * desc = "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/0/*)";
-    //char const * change = "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/1/*)";
+    char const * name = "test_wallet";
+    char const * desc = "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/0/*)";
+    char const * change = "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/1/*)";
     
     //printf("wallet name: %s\n", name);
     //printf("descriptor: %s\n", desc);
     //printf("change descriptor: %s\n", change);
-    //WalletPtr_t * wallet = new_wallet(name, desc, change);
-    //sync_wallet(wallet);
+    WalletPtr_t * wallet = new_wallet(name, desc, change);
     
-    //char const * address = new_address(wallet);
-    //printf("new address: %s\n", address);
+    sync_wallet(&wallet);    
+    sync_wallet(&wallet);
+    
+    char const * address1 = new_address(&wallet);
+    printf("address1: %s\n", address1);
+    char const * address2 = new_address(&wallet);
+    printf("address: %s\n", address2);
+    
+    //free_wallet(wallet);
+    //sync_wallet(&wallet);
         
     return EXIT_SUCCESS;
 }
