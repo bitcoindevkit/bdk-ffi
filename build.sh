8aa18fbf20fcf6ed0e9a94ded7f6e14e111c7006
Steve Myers <steve@notmandatory.org>
2021-06-08 18:15:20 -0700

    Test new, print, and free Config works

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 cargo build
 cargo test --features c-headers -- generate_headers
 cc main.c -o main -L target/debug -l bdk_ffi -l pthread -l dl -l m
+./main 
+
+# jvm
+mkdir -p jvm/build/jniLibs/x86_64_linux
+cp target/debug/libbdk_ffi.so jvm/build/jniLibs/x86_64_linux
+export LD_LIBRARY_PATH=`pwd`/target/debug
--- main.c
+++ main.c
@@ -4,40 +4,52 @@
 
 int main (int argc, char const * const argv[])
 {
-
+    // test print_string
+    print_string("hello 123");
+    
+    // test concat_string
     char const * string1 = "string1";
     char const * string2 = "string2";
     char * string3 = concat_string(string1, string2);
-    
-    print_string(string3);
     print_string(string3);
     free_string(string3);
-    //free_string(string3);
-
-    //Point_t a = new_point(84,45);
-    //Point_t b = new_point(0,39);
-    //Point_t m = mid_point(a, b);
-    //print_point(m);
+    // verify free_string after free_string fails
+    ////free_string(string3);
     
-    char const * name = "test_wallet";
-    char const * desc = "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/0/*)";
-    char const * change = "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/1/*)";
+    // test print_config with c created config
+    Config_t config1 = { .name = "test", .count = 101 };
+    print_config(&config1);
     
-    //printf("wallet name: %s\n", name);
-    //printf("descriptor: %s\n", desc);
-    //printf("change descriptor: %s\n", change);
-    WalletPtr_t * wallet = new_wallet(name, desc, change);
+    // test new_config
+    Config_t * config2 = new_config("test test", 202);
+    print_config(config2);
     
-    sync_wallet(&wallet);    
-    sync_wallet(&wallet);
+    // test free_config
+    free_config(config2);
+    // verify print_config after free_config fails (invalid data)
+    ////print_config(config2);
+    // verify free_config after free_config fails (double free detected, core dumped)
+    ////free_config(config2);
     
-    char const * address1 = new_address(&wallet);
-    printf("address1: %s\n", address1);
-    char const * address2 = new_address(&wallet);
-    printf("address: %s\n", address2);
+    //char const * name = "test_wallet";
+    //char const * desc = "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/0/*)";
+    //char const * change = "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/1/*)";
+    
+    ////printf("wallet name: %s\n", name);
+    ////printf("descriptor: %s\n", desc);
+    ////printf("change descriptor: %s\n", change);
+    //WalletPtr_t * wallet = new_wallet(name, desc, change);
+    
+    //sync_wallet(&wallet);    
+    //sync_wallet(&wallet);
+    
+    //char const * address1 = new_address(&wallet);
+    //printf("address1: %s\n", address1);
+    //char const * address2 = new_address(&wallet);
+    //printf("address: %s\n", address2);
     
     //free_wallet(wallet);
-    //sync_wallet(&wallet);
+    ////sync_wallet(&wallet);
         
     return EXIT_SUCCESS;
 }
