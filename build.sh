43ddf0a3b134c0d17d67e4874805a72b69059eb5
Steve Myers <steve@notmandatory.org>
2021-06-14 14:18:16 -0700

    Execute bdk_ffi_test via valgrind

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ cargo test --features c-headers -- generate_headers
 # cc
 export LD_LIBRARY_PATH=`pwd`/target/debug
 cc cc/bdk_ffi_test.c -o cc/bdk_ffi_test -L target/debug -l bdk_ffi -l pthread -l dl -l m
-#valgrind --leak-check=full cc/bdk_ffi_test
-cc/bdk_ffi_test
+valgrind --leak-check=full cc/bdk_ffi_test
+#cc/bdk_ffi_test
 
 # bdk-kotlin
 mkdir -p bdk-kotlin/jar/libs/x86_64_linux
