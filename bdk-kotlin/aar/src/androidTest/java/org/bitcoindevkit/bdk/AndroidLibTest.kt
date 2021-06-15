610d3939230f263a33a4c238e6c16f7bd3352a2c
Steve Myers <steve@notmandatory.org>
2021-06-14 22:38:29 -0700

    Add kotlin/aar android device tests

--- bdk-kotlin/aar/src/androidTest/java/org/bitcoindevkit/bdk/AndroidLibTest.kt
+++ bdk-kotlin/aar/src/androidTest/java/org/bitcoindevkit/bdk/AndroidLibTest.kt
@@ -1,14 +1,25 @@
 package org.bitcoindevkit.bdk
 
+import android.util.Log
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.sun.jna.Native
 import org.junit.*
-import org.junit.Assert.assertEquals
+
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+//import org.bitcoindevkit.bdkjni.Types.Network
+//import org.bitcoindevkit.bdkjni.Types.WalletConstructor
+//import org.bitcoindevkit.bdkjni.Types.WalletPtr
 
 /**
- * Library test, which will execute on linux host.
+ * Instrumented test, which will execute on an Android device.
  *
+ * See [testing documentation](http://d.android.com/tools/testing).
  */
-class LibTest {
+@RunWith(AndroidJUnit4::class)
+class AndroidLibTest {
 
     companion object {
         private val bdkFfi: Lib = Native.load("bdk_ffi", Lib::class.java)
@@ -24,26 +35,28 @@ class LibTest {
                 "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/1/*)"
 
             wallet = bdkFfi.new_wallet(name, desc, change)
-            println("wallet created")
+            Log.d("create_wallet", "wallet created")
         }
 
         @AfterClass
         @JvmStatic
         fun free_wallet() {
             bdkFfi.free_wallet(wallet)
-            println("wallet freed")
+            Log.d("free_wallet", "wallet freed")
         }
     }
 
     @Test
     fun sync() {
         bdkFfi.sync_wallet(wallet)
+        Log.d("sync", "wallet synced")
     }
 
     @Test
-    fun new_newaddress_wallet() {
+    fun new_address() {
         val address = bdkFfi.new_address(wallet)
         //println("address created from kotlin: $address")
         assertEquals(address, "tb1qzg4mckdh50nwdm9hkzq06528rsu73hjxxzem3e")
+        Log.d("new_address", "new address: $address")
     }
 }
---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -eo pipefail -o xtrace
+
+# rust
+cargo test --features c-headers -- generate_headers
+
+# cc
+export LD_LIBRARY_PATH=`pwd`/target/debug
+valgrind --leak-check=full cc/bdk_ffi_test
+#cc/bdk_ffi_test
+
+# bdk-kotlin
+(cd bdk-kotlin && gradle test)
+(cd bdk-kotlin && gradle :aar:connectedDebugAndroidTest)
