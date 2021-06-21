b57db8a7e7c848f732bedce1a860ba70d2eb739c
Steve Myers <steve@notmandatory.org>
2021-06-20 20:25:57 -0700

    Add kotlin test-fixtures module used by jvm and android

---
+++ bdk-kotlin/jvm/src/test/java/org/bitcoindevkit/bdk/JvmLibTest.kt
@@ -0,0 +1,15 @@
+package org.bitcoindevkit.bdk
+
+import com.sun.jna.Native
+import org.junit.*
+import org.junit.Assert.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+
+/**
+ * Library test, which will execute on linux host.
+ *
+ */
+class JvmLibTest : LibTest() {
+    
+}
