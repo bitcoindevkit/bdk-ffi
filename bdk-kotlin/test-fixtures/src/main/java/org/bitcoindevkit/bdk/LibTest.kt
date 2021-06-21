897edf164422807d9b026b265bbf013535a69308
Steve Myers <steve@notmandatory.org>
2021-06-20 23:39:42 -0700

    Add slf4j logging for kotlin jvm and android

--- bdk-kotlin/test-fixtures/src/main/java/org/bitcoindevkit/bdk/LibTest.kt
+++ bdk-kotlin/test-fixtures/src/main/java/org/bitcoindevkit/bdk/LibTest.kt
@@ -3,6 +3,8 @@ package org.bitcoindevkit.bdk
 import com.sun.jna.Native
 import org.junit.*
 import org.junit.Assert.assertEquals
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 
@@ -13,6 +15,8 @@ import kotlin.test.assertNull
 abstract class LibTest {
 
     companion object {
+        private val log: Logger = LoggerFactory.getLogger(LibTest::class.java)
+        
         private val bdkFfi: Lib = Native.load("bdk_ffi", Lib::class.java)
         private lateinit var wallet_result: Lib.WalletResult_t
 
@@ -26,26 +30,26 @@ abstract class LibTest {
                 "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/1/*)"
 
             wallet_result = bdkFfi.new_wallet_result(name, desc, change)
-            //println("wallet created")
+            log.debug("wallet created")
         }
 
         @AfterClass
         @JvmStatic
         fun free_wallet() {
             bdkFfi.free_wallet_result(wallet_result)
-            //println("wallet freed")
+            log.debug("wallet freed")
         }
     }
 
     @Test
     fun wallet_sync_error() {
         val bad_wallet_result = bdkFfi.new_wallet_result("test", "bad", null)
-        //println("wallet result created")
+        log.debug("wallet result created")
         val sync_result = bdkFfi.sync_wallet(bad_wallet_result)
         val sync_err_pointer = bdkFfi.get_void_err(sync_result)
         assertNotNull(sync_err_pointer)
         val sync_err = sync_err_pointer!!.getString(0)
-        //println("wallet sync error $sync_err")
+        log.debug("wallet sync error $sync_err")
     }
 
     @Test
@@ -64,7 +68,7 @@ abstract class LibTest {
         
         bdkFfi.free_string_result(address_result)
         bdkFfi.free_string(address_pointer)
-        //println("address created from kotlin: $address")
+        log.debug("address created from kotlin: $address")
         assertEquals(address, "tb1qzg4mckdh50nwdm9hkzq06528rsu73hjxxzem3e")
     }
 }
