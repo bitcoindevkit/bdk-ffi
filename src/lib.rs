9f29eb0e8684da25b15f39b5552e4bbd1ff00f20
Steve Myers <steve@notmandatory.org>
2021-06-21 17:01:53 -0700

    Add classes to wrap LibJna native types

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![deny(unsafe_code)] /* No `unsafe` needed! */
 
+mod error;
 mod wallet;
 
 /// The following test function is necessary for the header generation.
@@ -7,6 +8,7 @@ mod wallet;
 #[test]
 fn generate_headers() -> ::std::io::Result<()> {
     ::safer_ffi::headers::builder()
+        .with_guard("__RUST_BDK_FFI__")
         .to_file("cc/bdk_ffi.h")?
         .generate()
 }
--- src/wallet.rs
+++ src/wallet.rs
@@ -1,3 +1,4 @@
+use crate::error::get_name;
 use ::safer_ffi::prelude::*;
 use bdk::bitcoin::network::constants::Network::Testnet;
 use bdk::blockchain::{
@@ -13,7 +14,7 @@ use safer_ffi::char_p::{char_p_boxed, char_p_ref};
 #[derive_ReprC]
 #[ReprC::opaque]
 pub struct VoidResult {
-    raw: Result<(), String>,
+    raw: Result<(), Error>,
 }
 
 #[ffi_export]
@@ -22,7 +23,7 @@ fn get_void_err(void_result: &VoidResult) -> Option<char_p_boxed> {
         .raw
         .as_ref()
         .err()
-        .map(|s| char_p_boxed::try_from(s.clone()).unwrap())
+        .map(|e| char_p_boxed::try_from(get_name(e)).unwrap())
 }
 
 #[ffi_export]
@@ -33,7 +34,7 @@ fn free_void_result(void_result: Option<Box<VoidResult>>) {
 #[derive_ReprC]
 #[ReprC::opaque]
 pub struct StringResult {
-    raw: Result<String, String>,
+    raw: Result<String, Error>,
 }
 
 #[ffi_export]
@@ -51,7 +52,7 @@ fn get_string_err(string_result: &StringResult) -> Option<char_p_boxed> {
         .raw
         .as_ref()
         .err()
-        .map(|s| char_p_boxed::try_from(s.clone()).unwrap())
+        .map(|e| char_p_boxed::try_from(get_name(e)).unwrap())
 }
 
 #[ffi_export]
@@ -61,8 +62,19 @@ fn free_string_result(string_result: Option<Box<StringResult>>) {
 
 #[derive_ReprC]
 #[ReprC::opaque]
+pub struct WalletRef<'lt> {
+    raw: &'lt Wallet<AnyBlockchain, AnyDatabase>,
+}
+
+#[ffi_export]
+fn free_wallet_ref(wallet_ref: Option<Box<WalletRef>>) {
+    drop(wallet_ref)
+}
+
+#[derive_ReprC]
+#[ReprC::opaque]
 pub struct WalletResult {
-    raw: Result<Wallet<AnyBlockchain, AnyDatabase>, String>,
+    raw: Result<Wallet<AnyBlockchain, AnyDatabase>, Error>,
 }
 
 #[ffi_export]
@@ -74,12 +86,12 @@ fn new_wallet_result(
     let name = name.to_string();
     let descriptor = descriptor.to_string();
     let change_descriptor = change_descriptor.map(|s| s.to_string());
-    let wallet_result = new_wallet(name, descriptor, change_descriptor).map_err(|e| e.to_string());
+    let wallet_result = new_wallet(name, descriptor, change_descriptor);
     Box::new(WalletResult { raw: wallet_result })
 }
 
 fn new_wallet(
-    name: String,
+    _name: String,
     descriptor: String,
     change_descriptor: Option<String>,
 ) -> Result<Wallet<AnyBlockchain, AnyDatabase>, Error> {
@@ -108,24 +120,28 @@ fn get_wallet_err(wallet_result: &WalletResult) -> Option<char_p_boxed> {
         .raw
         .as_ref()
         .err()
-        .map(|s| char_p_boxed::try_from(s.clone()).unwrap())
+        .map(|e| char_p_boxed::try_from(get_name(&e)).unwrap())
 }
 
 #[ffi_export]
-fn sync_wallet(wallet_result: &WalletResult) -> Box<VoidResult> {
-    let wallet_result_ref = wallet_result.raw.as_ref().map_err(|error| error.clone());
-    let void_result = wallet_result_ref
-        .and_then(|w| w.sync(log_progress(), Some(100)).map_err(|e| e.to_string()));
+fn get_wallet_ok<'lt>(wallet_result: &'lt WalletResult) -> Option<Box<WalletRef<'lt>>> {
+    wallet_result
+        .raw
+        .as_ref()
+        .ok()
+        .map(|w| Box::new(WalletRef { raw: w}))
+}
+
+#[ffi_export]
+fn sync_wallet<'lt>(wallet_ref: &'lt WalletRef<'lt>) -> Box<VoidResult> {
+    let void_result = wallet_ref
+        .raw.sync(log_progress(), Some(100));
     Box::new(VoidResult { raw: void_result })
 }
 
 #[ffi_export]
-fn new_address(wallet_result: &WalletResult) -> Box<StringResult> {
-    let new_address = wallet_result
-        .raw
-        .as_ref()
-        .map_err(|error| error.clone())
-        .and_then(|w| w.get_address(New).map_err(|error| error.to_string()));
+fn new_address<'lt>(wallet_ref: &'lt WalletRef<'lt>) -> Box<StringResult> {
+    let new_address = wallet_ref.raw.get_address(New);
     let string_result = new_address.map(|a| a.to_string());
     Box::new(StringResult { raw: string_result })
 }
