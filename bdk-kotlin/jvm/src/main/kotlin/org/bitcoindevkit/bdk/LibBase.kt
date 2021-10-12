290db0105f007112a0ccf5391fd761bb82f80735
Steve Myers <steve@notmandatory.org>
2021-10-12 15:24:11 -0700

    [WIP] reorganize and remove old stuff

--- bdk-kotlin/jvm/src/main/kotlin/org/bitcoindevkit/bdk/LibBase.kt
+++
@@ -1,9 +0,0 @@
-package org.bitcoindevkit.bdk
-
-import com.sun.jna.Native
-
-abstract class LibBase {
-
-    protected val libJna: LibJna
-        get() = Native.load("bdk_ffi", LibJna::class.java)
-}
--- bdk-swift/bdk.swift/Sources/bdk.swift/bdk_swift.swift
+++
@@ -1,8 +0,0 @@
-import Clibbdkffi
-
-public struct bdk_swift {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
