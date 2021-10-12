0b265a7c2698e5e35b8c12b3d61b4388313fe7ee
Steve Myers <steve@notmandatory.org>
2021-10-11 23:04:18 -0700

    WIP -- NOT WORKING

--- build.sh
+++ build.sh
@@ -34,7 +34,7 @@ build_cc() {
 
 ## copy to bdk-kotlin
 copy_lib_kotlin() {
-  echo -n "Copy " 
+  echo -n "Copy "
   case $OS in
     "Darwin")
       echo -n "darwin "
@@ -59,18 +59,18 @@ build_kotlin() {
 build_android() {
   # If ANDROID_NDK_HOME is not set then set it to github actions default
   [ -z "$ANDROID_NDK_HOME" ] && export ANDROID_NDK_HOME=$ANDROID_HOME/ndk-bundle
-  
+
   # Update this line accordingly if you are not building *from* darwin-x86_64 or linux-x86_64
   export PATH=$PATH:$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/`uname | tr '[:upper:]' '[:lower:]'`-x86_64/bin
-  
+
   # Required for 'ring' dependency to cross-compile to Android platform, must be at least 21
   export CFLAGS="-D__ANDROID_API__=21"
-  
+
   # IMPORTANT: make sure every target is not a substring of a different one. We check for them with grep later on
   BUILD_TARGETS="${BUILD_TARGETS:-aarch64,armv7,x86_64,i686}"
-  
+
   mkdir -p bdk-kotlin/android/src/main/jniLibs/ bdk-kotlin/android/src/main/jniLibs/arm64-v8a bdk-kotlin/android/src/main/jniLibs/x86_64 bdk-kotlin/android/src/main/jniLibs/armeabi-v7a bdk-kotlin/android/src/main/jniLibs/x86
-  
+
   if echo $BUILD_TARGETS | grep "aarch64"; then
       CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="aarch64-linux-android21-clang" CC="aarch64-linux-android21-clang" cargo build --target=aarch64-linux-android
       cp target/aarch64-linux-android/debug/libbdk_ffi.so bdk-kotlin/android/src/main/jniLibs/arm64-v8a
@@ -88,13 +88,13 @@ build_android() {
       cp target/i686-linux-android/debug/libbdk_ffi.so bdk-kotlin/android/src/main/jniLibs/x86
   fi
 
-  # bdk-kotlin aar  
+  # bdk-kotlin aar
   (cd bdk-kotlin && ./gradlew :android:build && ./gradlew :android:publishToMavenLocal)
 }
 
 OS=$(uname)
 
-if [ $1 = "-h" ]
+if [ "$1" == "-h" ]
 then
   help
 else
