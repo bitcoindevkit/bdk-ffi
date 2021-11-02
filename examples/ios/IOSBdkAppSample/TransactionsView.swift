332ae148a6995d606e94027115ec41fb5a4504c2
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2021-11-02 22:47:17 +0530

    Simplify transactions

--- examples/ios/IOSBdkAppSample/TransactionsView.swift
+++ examples/ios/IOSBdkAppSample/TransactionsView.swift
@@ -7,16 +7,97 @@
 
 import SwiftUI
 
+extension Transaction {
+    public func getDetails() -> TransactionDetails {
+        switch self {
+        case .unconfirmed(let details): return details
+        case .confirmed(let details, _): return details
+        }
+    }
+}
+
+extension Date {
+   func getFormattedDate(format: String) -> String {
+        let dateformat = DateFormatter()
+        dateformat.dateFormat = format
+        return dateformat.string(from: self)
+    }
+}
+
+struct TransactionView: View {
+    var transaction: Transaction
+    
+    var body: some View {
+        switch transaction {
+        case .unconfirmed(let details):
+            VStack {
+                Text("Unconfirmed (\(details.id))")
+                    .multilineTextAlignment(.center)
+                    .frame(
+                        minWidth: 0,
+                        maxWidth: .infinity,
+                        minHeight: 0,
+                        alignment: .center
+                      )
+                VStack(alignment: .leading) {
+                    Text("sent: \(details.sent)")
+                    Text("received: \(details.received)")
+                    Text("fees: \(details.fees ?? 0)")
+                }.frame(
+                    minWidth: 0,
+                    maxWidth: .infinity,
+                    minHeight: 0,
+                    maxHeight: .infinity,
+                    alignment: .topLeading
+                  )
+            }.frame(
+                minWidth: 0,
+                maxWidth: .infinity,
+                minHeight: 0,
+                maxHeight: .infinity,
+                alignment: .topLeading
+              )
+        case .confirmed(let details, let confirmation):
+            VStack {
+                Text("Confirmed (\(details.id))")
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("sent: \(details.sent)")
+                        Text("received: \(details.received)")
+                        Text("fees: \(details.fees ?? 0)")
+                    }
+                    VStack(alignment: .trailing) {
+                        Text("height: \(confirmation.height)")
+                        Text(Date(timeIntervalSince1970: TimeInterval(confirmation.timestamp)).getFormattedDate(format: "yyyy-MM-dd HH:mm:ss"))
+                    }
+                }.frame(
+                    minWidth: 0,
+                    maxWidth: .infinity,
+                    minHeight: 0,
+                    maxHeight: .infinity,
+                    alignment: .topLeading
+                  )
+            }.frame(
+                minWidth: 0,
+                maxWidth: .infinity,
+                minHeight: 0,
+                maxHeight: .infinity,
+                alignment: .topLeading
+              )
+        }
+    }
+}
+
 struct TransactionsView: View {
     var transactions: [Transaction]
     
     var body: some View {
-        ScrollView {
+        List {
             if transactions.isEmpty {
                 Text("No transactions yet.").padding()
             } else {
-                ForEach(1..<transactions.count) { index in
-                    Text(String(describing: transactions[index])).padding()
+                ForEach(transactions, id: \.self) { transaction in
+                    TransactionView(transaction: transaction)
                 }
             }
         }.navigationBarTitle("Transactions")
@@ -25,6 +106,6 @@ struct TransactionsView: View {
 
 struct TransactionsView_Previews: PreviewProvider {
     static var previews: some View {
-        TransactionsView(transactions: [Transaction.unconfirmed(details: TransactionDetails(fees: nil, received: 1000, sent: 10000, id: "some-tx-id")), Transaction.confirmed(details: TransactionDetails(fees: nil, received: 1000, sent: 10000, id: "some-tx-id"), confirmation: Confirmation(height: 20087, timestamp: 1635863544))])
+        TransactionsView(transactions: [Transaction.unconfirmed(details: TransactionDetails(fees: nil, received: 1000, sent: 10000, id: "some-tx-id")), Transaction.confirmed(details: TransactionDetails(fees: nil, received: 1000, sent: 10000, id: "some-other-tx-id"), confirmation: Confirmation(height: 20087, timestamp: 1635863544))])
     }
 }
