68f266a17b29a84e8c63ed90126c5c152d2f51aa
Alekos Filini <alekos.filini@gmail.com>
2022-02-08 17:01:03 +0100

    Add a binary to generate bindings

---
+++ src/bin/generate.rs
@@ -0,0 +1,57 @@
+use std::env;
+use std::fs;
+use std::io::Write;
+use std::path::Path;
+
+const BDK_UDL: &str = "src/bdk.udl";
+
+fn fixup_python_lib_path<O: AsRef<Path>>(
+    out_dir: O,
+    lib_name: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    const LOAD_INDIRECT_DEF: &str = "def loadIndirect():";
+
+    let bindings_file = out_dir.as_ref().join("bdk.py");
+    let mut data = fs::read_to_string(&bindings_file)?;
+
+    let pos = data.find(LOAD_INDIRECT_DEF).expect(&format!(
+        "loadIndirect not found in `{}`",
+        bindings_file.display()
+    ));
+    let range = pos..pos + LOAD_INDIRECT_DEF.len();
+
+    let replacement = format!(
+        r#"
+def loadIndirect():
+    import glob
+    return getattr(ctypes.cdll, glob.glob(os.path.join(os.path.dirname(os.path.abspath(__file__)), '{}.*'))[0])
+
+def _loadIndirectOld():"#,
+        lib_name
+    );
+    data.replace_range(range, &replacement);
+
+    let mut file = fs::OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open(&bindings_file)?;
+    file.write(data.as_bytes())?;
+
+    Ok(())
+}
+
+fn generate_python() -> Result<(), Box<dyn std::error::Error>> {
+    let out_path = env::var("GENERATE_PYTHON_BINDINGS_OUT")
+        .map_err(|_| String::from("`GENERATE_PYTHON_BINDINGS_OUT` env variable missing"))?;
+    uniffi_bindgen::generate_bindings(&format!("{}/{}", env!("CARGO_MANIFEST_DIR"), BDK_UDL), None, vec!["python"], Some(&out_path), false)?;
+
+    if let Some(name) = env::var("GENERATE_PYTHON_BINDINGS_FIXUP_LIB_PATH").ok() {
+        fixup_python_lib_path(&out_path, &name)?;
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    generate_python()
+}
