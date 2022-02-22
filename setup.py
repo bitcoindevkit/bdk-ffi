4de866de209f6c863b3782ccc0957b9d3e5e7f78
thunderbiscuit <thunderbiscuit@protonmail.com>
2022-02-22 14:04:02 -0500

    Bump package version to 0.0.5

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ rust_ext = RustExtension(
 
 setup(
     name = 'bdkpython',
-    version = '0.1.0',
+    version = '0.0.5',
     description="The Python language bindings for the bitcoindevkit",
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
