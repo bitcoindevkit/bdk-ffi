b8f9d199a82fd43cb3406dba56d2ce4f3ed9bcbc
thunderbiscuit <thunderbiscuit@protonmail.com>
2022-09-08 18:21:04 -0400

    Simplify tests

--- android/src/androidTest/kotlin/org/bitcoindevkit/AndroidLibTest.kt
+++ android/src/androidTest/kotlin/org/bitcoindevkit/AndroidLibTest.kt
@@ -19,22 +19,29 @@ import java.io.File
 @RunWith(AndroidJUnit4::class)
 class AndroidLibTest {
 
-    fun getTestDataDir(): String {
+    private fun getTestDataDir(): String {
         val context = ApplicationProvider.getApplicationContext<Application>()
         return context.getDir("bdk-test", MODE_PRIVATE).toString()
     }
 
-    fun cleanupTestDataDir(testDataDir: String) {
+    private fun cleanupTestDataDir(testDataDir: String) {
         File(testDataDir).deleteRecursively()
     }
 
-    val log: Logger = LoggerFactory.getLogger(AndroidLibTest::class.java)
+    class LogProgress : Progress {
+        private val log: Logger = LoggerFactory.getLogger(AndroidLibTest::class.java)
 
-    val descriptor =
+        override fun update(progress: Float, message: String?) {
+            log.debug("Syncing...")
+        }
+    }
+
+    private val descriptor =
         "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/0/*)"
 
-    val databaseConfig = DatabaseConfig.Memory
-    val blockchainConfig = BlockchainConfig.Electrum(
+    private val databaseConfig = DatabaseConfig.Memory
+
+    private val blockchainConfig = BlockchainConfig.Electrum(
         ElectrumConfig(
             "ssl://electrum.blockstream.info:60002",
             null,
@@ -48,78 +55,27 @@ class AndroidLibTest {
     fun memoryWalletNewAddress() {
         val wallet = Wallet(descriptor, null, Network.TESTNET, databaseConfig)
         val address = wallet.getAddress(AddressIndex.NEW).address
-        assertNotNull(address)
         assertEquals("tb1qzg4mckdh50nwdm9hkzq06528rsu73hjxxzem3e", address)
-    }
-
-    @Test(expected = BdkException.Descriptor::class)
-    fun invalidDescriptorExceptionIsThrown() {
-        Wallet("invalid-descriptor", null, Network.TESTNET, databaseConfig)
     }
 
     @Test
-    fun sledWalletNewAddress() {
-        val testDataDir = getTestDataDir()
-        val databaseConfig = DatabaseConfig.Sled(SledDbConfiguration(testDataDir, "testdb"))
+    fun memoryWalletSyncGetBalance() {
         val wallet = Wallet(descriptor, null, Network.TESTNET, databaseConfig)
-        val address = wallet.getAddress(AddressIndex.NEW).address
-        assertNotNull(address)
-        assertEquals("tb1qzg4mckdh50nwdm9hkzq06528rsu73hjxxzem3e", address)
-        cleanupTestDataDir(testDataDir)
+        val blockchain = Blockchain(blockchainConfig)
+        wallet.sync(blockchain, LogProgress())
+        val balance: Balance = wallet.getBalance()
+        assertTrue(balance.total > 0u)
     }
 
     @Test
     fun sqliteWalletSyncGetBalance() {
-        val testDataDir = getTestDataDir()+"/bdk-wallet.sqlite"
+        val testDataDir = getTestDataDir() + "/bdk-wallet.sqlite"
         val databaseConfig = DatabaseConfig.Sqlite(SqliteDbConfiguration(testDataDir))
         val wallet = Wallet(descriptor, null, Network.TESTNET, databaseConfig)
         val blockchain = Blockchain(blockchainConfig)
         wallet.sync(blockchain, LogProgress())
-        val balance = wallet.getBalance()
-        assertTrue(balance > 0u)
+        val balance: Balance = wallet.getBalance()
+        assertTrue(balance.total > 0u)
         cleanupTestDataDir(testDataDir)
     }
-
-    @Test
-    fun onlineWalletInMemory() {
-        val blockchain = BlockchainConfig.Electrum(
-            ElectrumConfig(
-                "ssl://electrum.blockstream.info:60002",
-                null,
-                5u,
-                null,
-                100u
-            )
-        )
-        val wallet = Wallet(descriptor, null, Network.TESTNET, databaseConfig)
-        assertNotNull(wallet)
-        val network = wallet.getNetwork()
-        assertEquals(network, Network.TESTNET)
-    }
-
-    class LogProgress : Progress {
-        val log: Logger = LoggerFactory.getLogger(AndroidLibTest::class.java)
-
-        override fun update(progress: Float, message: String?) {
-            log.debug("Syncing...")
-        }
-    }
-
-    @Test
-    fun onlineWalletSyncGetBalance() {
-        val wallet = Wallet(descriptor, null, Network.TESTNET, databaseConfig)
-        val blockchain = Blockchain(blockchainConfig)
-        wallet.sync(blockchain, LogProgress())
-        val balance = wallet.getBalance()
-        assertTrue(balance > 0u)
-    }
-
-    @Test
-    fun validPsbtSerde() {
-        val validSerializedPsbt = "cHNidP8BAHUCAAAAASaBcTce3/KF6Tet7qSze3gADAVmy7OtZGQXE8pCFxv2AAAAAAD+////AtPf9QUAAAAAGXapFNDFmQPFusKGh2DpD9UhpGZap2UgiKwA4fUFAAAAABepFDVF5uM7gyxHBQ8k0+65PJwDlIvHh7MuEwAAAQD9pQEBAAAAAAECiaPHHqtNIOA3G7ukzGmPopXJRjr6Ljl/hTPMti+VZ+UBAAAAFxYAFL4Y0VKpsBIDna89p95PUzSe7LmF/////4b4qkOnHf8USIk6UwpyN+9rRgi7st0tAXHmOuxqSJC0AQAAABcWABT+Pp7xp0XpdNkCxDVZQ6vLNL1TU/////8CAMLrCwAAAAAZdqkUhc/xCX/Z4Ai7NK9wnGIZeziXikiIrHL++E4sAAAAF6kUM5cluiHv1irHU6m80GfWx6ajnQWHAkcwRAIgJxK+IuAnDzlPVoMR3HyppolwuAJf3TskAinwf4pfOiQCIAGLONfc0xTnNMkna9b7QPZzMlvEuqFEyADS8vAtsnZcASED0uFWdJQbrUqZY3LLh+GFbTZSYG2YVi/jnF6efkE/IQUCSDBFAiEA0SuFLYXc2WHS9fSrZgZU327tzHlMDDPOXMMJ/7X85Y0CIGczio4OFyXBl/saiK9Z9R5E5CVbIBZ8hoQDHAXR8lkqASECI7cr7vCWXRC+B3jv7NYfysb3mk6haTkzgHNEZPhPKrMAAAAAAAAA"
-        val psbt = PartiallySignedBitcoinTransaction(validSerializedPsbt)
-        val psbtSerialized = psbt.serialize()
-        assertEquals(psbtSerialized, validSerializedPsbt)
-    }
-
 }
