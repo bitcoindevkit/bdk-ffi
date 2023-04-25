89e85a20cf2ba6136e1652d549b0531332e730d5
thunderbiscuit <thunderbiscuit@protonmail.com>
2023-04-25 12:33:48 -0400

    Fix Python libraries' names to prepare for releasing

--- bdk-python/setup.py
+++ bdk-python/setup.py
@@ -62,4 +62,7 @@ setup(
     url="https://github.com/bitcoindevkit/bdk-ffi",
     author="Alekos Filini <alekos.filini@gmail.com>, Steve Myers <steve@notmandatory.org>",
     license="MIT or Apache 2.0",
+    # This is required to ensure the library name includes the python version, abi, and platform tags
+    # See issue #350 for more information
+    has_ext_modules=lambda: True,
 )
