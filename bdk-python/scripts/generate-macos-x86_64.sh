279847b0243559f630fabc02e4d62d6fa146f970
thunderbiscuit <thunderbiscuit@protonmail.com>
2023-06-15 13:53:06 -0400

    Use macos-13 image to test and publish Python libraries

---
+++ bdk-python/scripts/generate-macos-x86_64.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+python3 --version
+pip install --user -r requirements.txt
+
+echo "Generating bdk.py..."
+cd ../bdk-ffi/
+cargo run --bin uniffi-bindgen generate src/bdk.udl --language python --out-dir ../bdk-python/src/bdkpython/ --no-format
+
+echo "Generating native binaries..."
+rustup target add x86_64-apple-darwin
+cargo build --profile release-smaller --target x86_64-apple-darwin
+
+echo "Copying libraries libbdkffi.dylib..."
+cp ../target/x86_64-apple-darwin/release-smaller/libbdkffi.dylib ../bdk-python/src/bdkpython/libbdkffi.dylib
+
+echo "All done!"
