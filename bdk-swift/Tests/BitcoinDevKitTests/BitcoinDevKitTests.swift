04e7c3ebd7f5be647ef7b8e2f1203868b6e2e067
Matthew <ramsden@hey.com>
2023-07-18 12:22:21 -0500

    swift: update testMemoryWalletNewAddress unit test

--- bdk-swift/Tests/BitcoinDevKitTests/BitcoinDevKitTests.swift
+++ bdk-swift/Tests/BitcoinDevKitTests/BitcoinDevKitTests.swift
@@ -3,20 +3,9 @@ import XCTest
 
 final class BitcoinDevKitTests: XCTestCase {
     
-    func testMemoryWalletNewAddress() throws {
-        let desc = try Descriptor(
-            descriptor: "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/0/*)",
-            network: Network.regtest
-        )
-        let databaseConfig = DatabaseConfig.memory
-        let wallet = try Wallet.init(descriptor: desc, changeDescriptor: nil, network: Network.regtest, databaseConfig: databaseConfig)
-        let addressInfo = try wallet.getAddress(addressIndex: AddressIndex.new)
-        XCTAssertEqual(addressInfo.address.asString(), "bcrt1qzg4mckdh50nwdm9hkzq06528rsu73hjxytqkxs")
-    }
-    
     private let descriptor = "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/0/*)"
-    let databaseConfig = DatabaseConfig.memory
-    let blockchainConfig = BlockchainConfig.electrum(
+    private let databaseConfig = DatabaseConfig.memory
+    private let blockchainConfig = BlockchainConfig.electrum(
         config: ElectrumConfig(
             url: "ssl://electrum.blockstream.info:60002",
             socks5: nil,
@@ -27,13 +16,34 @@ final class BitcoinDevKitTests: XCTestCase {
         )
     )
     
+    func testMemoryWalletNewAddress() throws {
+        let descriptor = try Descriptor(
+            descriptor: descriptor,
+            network: .testnet
+        )
+        let wallet = try Wallet(
+            descriptor: descriptor,
+            changeDescriptor: nil,
+            network: .testnet,
+            databaseConfig: databaseConfig
+        )
+        let addressInfo = try wallet.getAddress(addressIndex: AddressIndex.new)
+        
+        XCTAssertEqual(addressInfo.address.asString(), "tb1qzg4mckdh50nwdm9hkzq06528rsu73hjxxzem3e")
+    }
+    
     func testMemoryWalletBalance() throws {
         let descriptor = try Descriptor(
             descriptor: descriptor,
             network: .testnet
         )
+        let wallet = try Wallet(
+            descriptor: descriptor,
+            changeDescriptor: nil,
+            network: .testnet,
+            databaseConfig: databaseConfig
+        )
         let blockchain = try Blockchain(config: blockchainConfig)
-        let wallet = try Wallet(descriptor: descriptor, changeDescriptor: nil, network: Network.testnet, databaseConfig: databaseConfig)
         try wallet.sync(
             blockchain: blockchain,
             progress: nil
