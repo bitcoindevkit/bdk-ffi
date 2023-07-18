33b78c7832acbb96b5151a6db29b8f9e59a97b95
Matthew <ramsden@hey.com>
2023-07-18 11:49:17 -0500

    swift: adding unit tests

--- bdk-swift/Tests/BitcoinDevKitTests/BitcoinDevKitTests.swift
+++ bdk-swift/Tests/BitcoinDevKitTests/BitcoinDevKitTests.swift
@@ -2,6 +2,7 @@ import XCTest
 @testable import BitcoinDevKit
 
 final class BitcoinDevKitTests: XCTestCase {
+    
     func testMemoryWalletNewAddress() throws {
         let desc = try Descriptor(
             descriptor: "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/0/*)",
@@ -12,4 +13,35 @@ final class BitcoinDevKitTests: XCTestCase {
         let addressInfo = try wallet.getAddress(addressIndex: AddressIndex.new)
         XCTAssertEqual(addressInfo.address.asString(), "bcrt1qzg4mckdh50nwdm9hkzq06528rsu73hjxytqkxs")
     }
+    
+    private let descriptor = "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/0/*)"
+    let databaseConfig = DatabaseConfig.memory
+    let blockchainConfig = BlockchainConfig.electrum(
+        config: ElectrumConfig(
+            url: "ssl://electrum.blockstream.info:60002",
+            socks5: nil,
+            retry: 5,
+            timeout: nil,
+            stopGap: 100,
+            validateDomain: true
+        )
+    )
+    
+    func testMemoryWalletBalance() throws {
+        let descriptor = try Descriptor(
+            descriptor: descriptor,
+            network: .testnet
+        )
+        let blockchain = try Blockchain(config: blockchainConfig)
+        let wallet = try Wallet(descriptor: descriptor, changeDescriptor: nil, network: Network.testnet, databaseConfig: databaseConfig)
+        try wallet.sync(
+            blockchain: blockchain,
+            progress: nil
+        )
+        let balance = try wallet.getBalance().total
+        
+        XCTAssertTrue(balance > 0)
+    }
+    
+    
 }
