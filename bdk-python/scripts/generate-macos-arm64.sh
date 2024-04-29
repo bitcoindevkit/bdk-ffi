431ab90f04e6ae6ed100dcd89d088e808489e3f9
thunderbiscuit <thunderbiscuit@protonmail.com>
2024-04-29 15:23:11 -0400

    fix: set rust compiler version before running cargo commands in python

--- bdk-python/scripts/generate-macos-arm64.sh
+++ bdk-python/scripts/generate-macos-arm64.sh
@@ -6,10 +6,10 @@ pip install -r requirements.txt
 
 echo "Generating bdk.py..."
 cd ../bdk-ffi/
+rustup default 1.77.1
 cargo run --bin uniffi-bindgen generate src/bdk.udl --language python --out-dir ../bdk-python/src/bdkpython/ --no-format
 
 echo "Generating native binaries..."
-rustup default 1.77.1
 rustup target add aarch64-apple-darwin
 cargo build --profile release-smaller --target aarch64-apple-darwin
 
