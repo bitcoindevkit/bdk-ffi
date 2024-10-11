bb9f09e76605b85da29161219bd1f39017dfbad0
Matthew <ramsden@hey.com>
2024-10-11 12:37:19 -0500

    chore: update libraries to official release versions

--- bdk-python/setup.py
+++ bdk-python/setup.py
@@ -18,7 +18,7 @@ import bdkpython as bdk
 
 setup(
     name="bdkpython",
-    version="1.0.0b5.dev",
+    version="1.0.0b5",
     description="The Python language bindings for the Bitcoin Development Kit",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
