dfb28553a9f2d54bda586138b672ffbb37c01cbb
thunderbiscuit <thunderbiscuit@protonmail.com>
2025-05-15 10:29:26 -0400

    refactor: migrate policy-related types to procedural macros

--- bdk-ffi/src/types.rs
+++ bdk-ffi/src/types.rs
@@ -335,12 +335,13 @@ pub struct KeychainAndIndex {
 }
 
 /// Descriptor spending policy
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone, uniffi::Object)]
 pub struct Policy(BdkPolicy);
 
 impl_from_core_type!(BdkPolicy, Policy);
 impl_into_core_type!(Policy, BdkPolicy);
 
+#[uniffi::export]
 impl Policy {
     pub fn id(&self) -> String {
         self.0.id.clone()
@@ -367,7 +368,7 @@ impl Policy {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, uniffi::Enum)]
 pub enum SatisfiableItem {
     EcdsaSignature {
         key: PkOrF,
@@ -500,7 +501,7 @@ impl TryFrom<&LockTime> for BdkLockTime {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, uniffi::Enum)]
 pub enum Satisfaction {
     Partial {
         n: u64,
