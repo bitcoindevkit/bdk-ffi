6ee7c3f95c61772f08b406e31c563a893eb82036
Matthew <ramsden@hey.com>
2025-06-26 17:11:43 -0500

    refactor: use macros for `PsbtParseError`

--- bdk-ffi/src/lib.rs
+++ bdk-ffi/src/lib.rs
@@ -26,7 +26,6 @@ use crate::error::MiniscriptError;
 use crate::error::ParseAmountError;
 use crate::error::PsbtError;
 use crate::error::PsbtFinalizeError;
-use crate::error::PsbtParseError;
 use crate::error::RequestBuilderError;
 use crate::error::TransactionError;
 use crate::types::FullScanRequest;
