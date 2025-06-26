1b30a60fcd8980b72c9a62013732319b6065f989
Matthew <ramsden@hey.com>
2025-06-26 17:46:23 -0500

    refactor: use macros for `ElectrumError`

--- bdk-ffi/src/lib.rs
+++ bdk-ffi/src/lib.rs
@@ -14,7 +14,6 @@ mod wallet;
 use crate::bitcoin::FeeRate;
 use crate::bitcoin::OutPoint;
 use crate::bitcoin::Script;
-use crate::error::ElectrumError;
 use crate::error::EsploraError;
 use crate::error::PsbtFinalizeError;
 use crate::error::TransactionError;
