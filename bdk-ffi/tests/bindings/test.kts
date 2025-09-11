35dcab1fd60db0eccf8a864918558ad98c72041b
thunderbiscuit <thunderbiscuit@protonmail.com>
2025-09-11 09:42:02 -0400

    test: remove uniffi build_foreign_language_testcases tests

--- bdk-ffi/tests/bindings/test.kts
+++
@@ -1,9 +0,0 @@
-/*
- * This is a basic test kotlin program that does nothing but confirm that the kotlin bindings compile
- * and that a program that depends on them will run.
- */
-
-import org.bitcoindevkit.bitcoin.Network
-
-// A type from bitcoin-ffi
-val network = Network.TESTNET
