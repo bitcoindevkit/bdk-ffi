22d7170fd8343ac0731ab0c1dccf2a7d15a5570e
thunderbiscuit <thunderbiscuit@protonmail.com>
2026-02-17 15:32:43 -0500

    build: upgrade the android build scripts and workflow

--- bdk-android/build.gradle.kts
+++ bdk-android/build.gradle.kts
@@ -1,13 +1,8 @@
 plugins {
-    id("com.android.library").version("8.3.1").apply(false)
+    id("com.android.library").version("8.13.2").apply(false)
     id("org.jetbrains.kotlin.android").version("2.1.10").apply(false)
     id("org.gradle.maven-publish")
     id("org.gradle.signing")
-    id("org.jetbrains.dokka").version("2.0.0").apply(false)
-    id("org.jetbrains.dokka-javadoc").version("2.0.0").apply(false)
+    id("org.jetbrains.dokka").version("2.1.0").apply(false)
+    id("org.jetbrains.dokka-javadoc").version("2.1.0").apply(false)
 }
-
-// library version is defined in gradle.properties
-val libraryVersion: String by project
-group = "org.bitcoindevkit"
-version = libraryVersion
