39feaf0af11506b2cb9092c8d8385d5f2294b822
thunderbiscuit <thunderbiscuit@protonmail.com>
2026-02-17 21:25:40 -0500

    build: use com.vanniktech.maven.publish plugin for android publication

--- bdk-android/build.gradle.kts
+++ bdk-android/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     id("com.android.library").version("8.13.2").apply(false)
     id("org.jetbrains.kotlin.android").version("2.1.10").apply(false)
-    id("org.gradle.maven-publish")
-    id("org.gradle.signing")
     id("org.jetbrains.dokka").version("2.1.0").apply(false)
-    id("org.jetbrains.dokka-javadoc").version("2.1.0").apply(false)
+    id("com.vanniktech.maven.publish").version("0.36.0").apply(false)
 }
