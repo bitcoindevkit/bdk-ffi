e0454c2d9d3737ac12d023197376c6183f8c37b3
Matthew <ramsden@hey.com>
2026-05-21 15:33:38 -0500

    feat: add electrum client retry option

--- bdk-ffi/src/electrum.rs
+++ bdk-ffi/src/electrum.rs
@@ -30,18 +30,23 @@ impl ElectrumClient {
     /// Creates a new bdk client from a electrum_client::ElectrumApi
     /// Optional: Set the proxy of the builder
     /// Optional: Set the timeout of the builder
+    /// Optional: Set the retry attempts number of the builder
     /// Optional: Set whether the server's TLS certificate is validated.
-    #[uniffi::constructor(default(socks5 = None, timeout = None, validate_domain = true))]
+    #[uniffi::constructor(default(socks5 = None, timeout = None, retry = None, validate_domain = true))]
     pub fn new(
         url: String,
         socks5: Option<String>,
         timeout: Option<u8>,
+        retry: Option<u8>,
         validate_domain: bool,
     ) -> Result<Self, ElectrumError> {
         let mut config = bdk_electrum::electrum_client::ConfigBuilder::new();
         config = config.validate_domain(validate_domain);
         if let Some(timeout) = timeout {
             config = config.timeout(Some(timeout));
+        }
+        if let Some(retry) = retry {
+            config = config.retry(retry);
         }
         if let Some(socks5) = socks5 {
             config = config.socks5(Some(bdk_electrum::electrum_client::Socks5Config::new(
