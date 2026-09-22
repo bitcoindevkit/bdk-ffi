c4b7862bd738cefe350dc443a2c8c206f816a5a3
Matthew Ramsden <6657488+reez@users.noreply.github.com>
2026-09-22 09:16:25 -0500

    fix(build): enforce locked dependency resolution

--- bdk-android/scripts/dev/build-dev-macos-aarch64.sh
+++ bdk-android/scripts/dev/build-dev-macos-aarch64.sh
@@ -18,11 +18,11 @@ rustup target add $COMPILATION_TARGET_ARM64_V8A
 
 # Build the binaries
 # The CC and CARGO_TARGET_<TARGET>_LINUX_ANDROID_LINKER environment variables must be declared on the same line as the cargo build command
-CC="aarch64-linux-android24-clang" CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="aarch64-linux-android24-clang" cargo build --target $COMPILATION_TARGET_ARM64_V8A
+CC="aarch64-linux-android24-clang" CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="aarch64-linux-android24-clang" cargo build --locked --target $COMPILATION_TARGET_ARM64_V8A
 
 # Copy the binaries to their respective resource directories
 mkdir -p ../bdk-android/lib/src/main/jniLibs/$RESOURCE_DIR_ARM64_V8A/
 cp ./target/$COMPILATION_TARGET_ARM64_V8A/debug/$LIB_NAME ../bdk-android/lib/src/main/jniLibs/$RESOURCE_DIR_ARM64_V8A/
 
 # Generate Kotlin bindings using uniffi-bindgen
-cargo run --bin uniffi-bindgen generate --library ./target/$COMPILATION_TARGET_ARM64_V8A/debug/$LIB_NAME --language kotlin --out-dir ../bdk-android/lib/src/main/kotlin/ --no-format
+cargo run --locked --bin uniffi-bindgen generate --library ./target/$COMPILATION_TARGET_ARM64_V8A/debug/$LIB_NAME --language kotlin --out-dir ../bdk-android/lib/src/main/kotlin/ --no-format
